e5994b3e2735fa6170abd9931b4d5fbb6cc05cd9
Padma Patil <padma@email.com>
2019-01-18 10:06:53 -0800

    add dynamic information to url call to API, and a return message should an error occur.

--- src/doctor.js
+++ src/doctor.js
@@ -7,12 +7,13 @@ class Doctor {
 searchAPIforDoctorInfo(name, firstName, lastName, query) {
   return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=John%20Smith&first_name=John&last_name=Smith&query=Toothache&location=45.512230%2C-122.658722%2C10&user_location=45.512230%2C-122.658722&sort=best-match-asc&skip=0&limit=100&user_key=5145ff681cdae63a7a950e576a5bc720`;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&first_name=${firstName}&last_name=${lastName}&query=${query}&location=45.512230%2C-122.658722%2C10&user_location=45.512230%2C-122.658722&sort=best-match-asc&skip=0&limit=100&user_key=5145ff681cdae63a7a950e576a5bc720`;
     request.onload = function () {
       if (this.status === 200){
         resolve(request.response);
       } else {
         reject(Error(request.statusText));
+        return `You have the following error ${request.statusText}.`;
       }
     }
 
